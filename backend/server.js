12396c7441637c70ccce1f1342e7e2b5d0c53dee
Breskella Boshra <breskella.boshra27@gmail.com>
2026-06-13 21:54:22 +0300

    fix cors settings

--- backend/server.js
+++ backend/server.js
@@ -15,8 +15,10 @@ const port = process.env.PORT || 4000
 
 // middleware
 app.use(express.json())
-app.use(cors())
-
+app.use(cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true
+}));
 //db connection
 connectDB();
 
