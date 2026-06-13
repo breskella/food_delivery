65207dd5789a570e445d306e8a93d46065bdb72e
Breskella Boshra <breskella.boshra27@gmail.com>
2026-06-14 02:26:21 +0300

    update db connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,6 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
     try {
-        console.log("Attempting to connect to:", process.env.MONGODB_URI ? "URI is set" : "URI is MISSING");
         await mongoose.connect(process.env.MONGODB_URI);
         console.log("DB Connected");
     } catch (error) {
