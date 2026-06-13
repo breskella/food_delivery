d877239a8108bfcbcca80c84750b1f98959fd7d2
Breskella Boshra <breskella.boshra27@gmail.com>
2026-06-14 02:12:32 +0300

    Update db connection to use process.env

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
     try {
-        await mongoose.connect("mongodb://127.0.0.1:27017/food-del");
+        await mongoose.connect(process.env.MONGODB_URI);
         console.log("DB Connected");
     } catch (error) {
         console.log("DB Connection Error:", error);
